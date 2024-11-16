e22a2d3e129bce0c656680f7fe5dde6a5298abab
MonetteParangat <monette.parangat@ibm.com>
2024-11-16 20:17:12 +0800

    setup api axios on product

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,60 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useAccordionButton } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+
 function Products() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const productLoad = async () => {
+        const response = await axios.get('/data.json')
+            .then(response => {
+                setProducts(response.data.products);
+                setLoading(false);
+            }).catch(error => {
+                console.error("Error fetching data: ", error);
+                setLoading(false);
+            });
+
+        return response
+    }
+
+    useEffect(() => {
+        productLoad();
+    }, []);
+
     return (
-        <div>
-            Products here eheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        <div className="container-products">
+            <div className="wrapper-table">
+                <Table striped hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {loading ? (
+                            <tr>
+                                <td colSpan={4}>...loading</td>
+                            </tr>
+                        ) : (
+                            products.map((product, i) => (
+                                <tr key={i}>
+                                    <td>{product.id}</td>
+                                    <td>{product.name}</td>
+                                    <td>{product.description}</td>
+                                    <td>{product.price}</td>
+                                </tr>
+                            ))
+                        )}
+
+                    </tbody>
+                </Table>
+            </div>
         </div>
     );
 }
